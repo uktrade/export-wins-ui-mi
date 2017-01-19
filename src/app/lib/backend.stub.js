fb376b5142590c21335f7177cb25d98f6b41276f
Graham Veal <graham.veal@digital.trade.gov.uk>
2017-01-19 15:44:48 +0000

    Refactor stubs to make it cleaner

--- src/app/lib/backend.stub.js
+++ src/app/lib/backend.stub.js
@@ -1,67 +1,32 @@
-const isSectorTeamsOverview = /^\/mi\/sector_teams\/overview\/$/;
-const isSectorTeamTopNonHvc = /^\/mi\/sector_teams\/[0-9]+\/top_non_hvcs\/$/;
-const isSectorTeamCampaign = /^\/mi\/sector_teams\/[0-9]+\/campaigns\/$/;
-const isSectorTeamMonths = /^\/mi\/sector_teams\/[0-9]+\/months\/$/;
-const isSectorTeam = /^\/mi\/sector_teams\/[0-9]+\/$/;
-const isSectorTeams = /^\/mi\/sector_teams\/$/;
 
-const isOverseasRegionTopNonHvc = /^\/mi\/os_regions\/[0-9]+\/top_non_hvcs\/$/;
-const isOverseasRegionCampaigns = /^\/mi\/os_regions\/[0-9]+\/campaigns\/$/;
-const isOverseasRegionMonths = /^\/mi\/os_regions\/[0-9]+\/months\/$/;
-const isOverseasRegion = /^\/mi\/os_regions\/[0-9]+\/$/;
-const isOverseasRegions = /^\/mi\/os_regions\/$/;
-const isOverseasRegionOverview = /^\/mi\/os_regions\/overview\/$/;
+function getStub( path ){
 
-const isParentSectors = /^\/mi\/parent_sectors\/$/;
-const isParentSector = /^\/mi\/parent_sectors\/[0-9]+\/$/;
-const isParentCampaigns = /^\/mi\/parent_sectors\/[0-9]+\/campaigns\/$/;
-const isParentMonths = /^\/mi\/parent_sectors\/[0-9]+\/months\/$/;
-const isParentTopNonHvcs = /^\/mi\/parent_sectors\/[0-9]+\/top_non_hvcs\/$/;
-
-
-const sectorTeamsOverviewStub = require( '../../stubs/backend/sector_teams/overview' );
-const SectorTeamTopNonHvcStub = require( '../../stubs/backend/sector_teams/top_non_hvcs' );
-const sectorTeamCampaignStub = require( '../../stubs/backend/sector_teams/campaigns' );
-const sectorTeamMonthsStub = require( '../../stubs/backend/sector_teams/months_2016-12-12' );
-const sectorTeamStub = require( '../../stubs/backend/sector_teams/sector_team_v2' );
-const sectorTeamsStub = require( '../../stubs/backend/sector_teams/' );
-
-const overseasRegionTopNonHvcStub = require( '../../stubs/backend/os_regions/top_non_hvcs' );
-const overseasRegionCampaignsStub = require( '../../stubs/backend/os_regions/campaigns' );
-const overseasRegionMonthsStub = require( '../../stubs/backend/os_regions/months' );
-const overseasRegionStub = require( '../../stubs/backend/os_regions/region' );
-const overseasRegionsStub = require( '../../stubs/backend/os_regions/' );
-const overseasRegionsOverviewStub = require( '../../stubs/backend/os_regions/overview' );
-
-const parentSectorsStub = require( '../../stubs/backend/parent_sectors' );
-const parentSectorStub = require( '../../stubs/backend/parent_sector' );
-const parentCampaignsStub = require( '../../stubs/backend/parent_sector_campaigns' );
-const parentMonthsStub = require( '../../stubs/backend/parent_sector_months' );
-const parentTopNonHvcsStub = require( '../../stubs/backend/parent_sector_top_non_hvcs' );
+	return require( '../../stubs/backend/' + path );
+}
 
 const response = { statusCode: 200, isSuccess: true };
 
 const stubs = [
 
-	[ isSectorTeamsOverview, sectorTeamsOverviewStub ],
-	[ isSectorTeamTopNonHvc, SectorTeamTopNonHvcStub ],
-	[ isSectorTeamCampaign, sectorTeamCampaignStub ],
-	[ isSectorTeamMonths, sectorTeamMonthsStub ],
-	[ isSectorTeam, sectorTeamStub ],
-	[ isSectorTeams, sectorTeamsStub ],
+	[ /^\/mi\/sector_teams\/overview\/$/, getStub( 'sector_teams/overview' ) ],
+	[ /^\/mi\/sector_teams\/[0-9]+\/top_non_hvcs\/$/, getStub( 'sector_teams/top_non_hvcs' ) ],
+	[ /^\/mi\/sector_teams\/[0-9]+\/campaigns\/$/, getStub( 'sector_teams/campaigns' ) ],
+	[ /^\/mi\/sector_teams\/[0-9]+\/months\/$/, getStub( 'sector_teams/months_2016-12-12' ) ],
+	[ /^\/mi\/sector_teams\/[0-9]+\/$/, getStub( 'sector_teams/sector_team_v2' ) ],
+	[ /^\/mi\/sector_teams\/$/, getStub( 'sector_teams' ) ],
 
-	[ isOverseasRegionTopNonHvc, overseasRegionTopNonHvcStub ],
-	[ isOverseasRegionCampaigns, overseasRegionCampaignsStub ],
-	[ isOverseasRegionMonths, overseasRegionMonthsStub ],
-	[ isOverseasRegion, overseasRegionStub ],
-	[ isOverseasRegions, overseasRegionsStub ],
-	[ isOverseasRegionOverview, overseasRegionsOverviewStub ],
+	[ /^\/mi\/os_regions\/[0-9]+\/top_non_hvcs\/$/, getStub( 'os_regions/top_non_hvcs' ) ],
+	[ /^\/mi\/os_regions\/[0-9]+\/campaigns\/$/, getStub( 'os_regions/campaigns' ) ],
+	[ /^\/mi\/os_regions\/[0-9]+\/months\/$/, getStub( 'os_regions/months' ) ],
+	[ /^\/mi\/os_regions\/[0-9]+\/$/, getStub( 'os_regions/region' ) ],
+	[ /^\/mi\/os_regions\/$/, getStub( 'os_regions' ) ],
+	[ /^\/mi\/os_regions\/overview\/$/, getStub( 'os_regions/overview' ) ],
 
-	[ isParentSectors, parentSectorsStub ],
-	[ isParentSector, parentSectorStub ],
-	[ isParentCampaigns, parentCampaignsStub ],
-	[ isParentMonths, parentMonthsStub ],
-	[ isParentTopNonHvcs, parentTopNonHvcsStub ]
+	[ /^\/mi\/parent_sectors\/$/, getStub( 'parent_sectors' ) ],
+	[ /^\/mi\/parent_sectors\/[0-9]+\/$/, getStub( 'parent_sector' ) ],
+	[ /^\/mi\/parent_sectors\/[0-9]+\/campaigns\/$/, getStub( 'parent_sector_campaigns' ) ],
+	[ /^\/mi\/parent_sectors\/[0-9]+\/months\/$/, getStub( 'parent_sector_months' ) ],
+	[ /^\/mi\/parent_sectors\/[0-9]+\/top_non_hvcs\/$/, getStub( 'parent_sector_top_non_hvcs' ) ]
 ];
 
 module.exports = {
