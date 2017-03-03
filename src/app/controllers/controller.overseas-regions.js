9dfdeae5eaa02584eea9c9bd5748a8c69dabeba0
Graham <grahamveal@gmail.com>
2017-03-03 17:09:48 +0000

    Release v2.3.2 (#11)
    * Output correct total
    * 2.3.2

--- src/app/controllers/controller.overseas-regions.js
+++ src/app/controllers/controller.overseas-regions.js
@@ -47,7 +47,7 @@ module.exports = {
 				winSummary: {
 					target: winsData.hvcs.target,
 					totalConfirmed: winsData.wins.export.hvc.value.confirmed,
-					totalUnconfirmed: winsData.wins.export.hvc.value.unconfirmed,
+					total: winsData.wins.export.hvc.value.total,
 					progress: targetProgressDataSet.create( winsData ),
 					averageTimeToConfirm: winsData.avg_time_to_confirm,
 					exportValue: winsData.exportValue,
