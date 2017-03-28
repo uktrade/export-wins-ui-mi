c17d582825611b2fe383cb65fbb32bfaebbfa59e
Graham <grahamveal@gmail.com>
2017-03-28 07:43:05 +0100

    Release v2.5.1 (#25)
    * Ensure date stamps don't go past 31st March 2017
    * 2.5.1

--- src/test/app/specs/lib/nunjucks-filters/date-with-time.spec.js
+++ src/test/app/specs/lib/nunjucks-filters/date-with-time.spec.js
@@ -2,8 +2,30 @@ const dateWithTime = require( '../../../../../app/lib/nunjucks-filters/date-with
 
 describe( 'Date with time filter', function(){
 
-	it( 'Should return the date and the time', function(){
+	describe( 'With just a date', function(){
+	
+		it( 'Should return the date and the time', function(){
 
-		expect( dateWithTime( new Date( 1487928284460 ) ) ).toEqual( '9:24am 24 February 2017' );
+			expect( dateWithTime( new Date( 1487928284460 ) ) ).toEqual( '9:24am 24 February 2017' );
+		} );
+	} );
+
+	describe( 'With a date and a max date', function(){
+
+		describe( 'When the date is lower than the maxDate', function(){
+		
+			it( 'Should return the date', function(){
+		
+				expect( dateWithTime( new Date( 1487928284460 ), '2017-03-31:23:59' ) ).toEqual( '9:24am 24 February 2017' );
+			} );
+		} );
+
+		describe( 'When the max date is greater than the date', function(){
+		
+			it( 'Should return the max date', function(){
+		
+				expect( dateWithTime( new Date( 1517443200000 ), '2017-03-31:23:59' ) ).toEqual( '11:59pm 31 March 2017' );
+			} );
+		} );
 	} );
 } );
