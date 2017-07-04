76d8be32d9fd8a6f6322a9603c2059fbc328d9cf
Graham Veal <graham.veal@digital.trade.gov.uk>
2017-07-04 13:13:20 +0100

    Remove yellow bg and make text bold - Add more diff tests for backstop - Set timezone to GMT

--- src/test/app/specs/lib/nunjucks-filters/date-with-time.spec.js
+++ src/test/app/specs/lib/nunjucks-filters/date-with-time.spec.js
@@ -11,13 +11,13 @@ describe( 'Date with time filter', function(){
 
 				it( 'Should return the correct date and time', function(){
 
-					expect( dateWithTime( new Date( 1491004799 * 1000 ) ) ).toEqual( '11:59pm 31 March 2017' );
+					expect( dateWithTime( new Date( 1491004799 * 1000 ) ) ).toEqual( '11:59pm 31 March 2017 GMT' );
 				} );
 			} );
 
 			it( 'Should return the date and the time', function(){
 
-				expect( dateWithTime( new Date( 1487928284460 ) ) ).toEqual( '9:24am 24 February 2017' );
+				expect( dateWithTime( new Date( 1487928284460 ) ) ).toEqual( '9:24am 24 February 2017 GMT' );
 			} );
 		} );
 
@@ -27,7 +27,7 @@ describe( 'Date with time filter', function(){
 
 				it( 'Should return the date', function(){
 
-					expect( dateWithTime( new Date( 1487928284460 ), '2017-03-31:23:59 GMT' ) ).toEqual( '9:24am 24 February 2017' );
+					expect( dateWithTime( new Date( 1487928284460 ), '2017-03-31:23:59 GMT' ) ).toEqual( '9:24am 24 February 2017 GMT' );
 				} );
 			} );
 
@@ -35,7 +35,7 @@ describe( 'Date with time filter', function(){
 
 				it( 'Should return the max date', function(){
 
-					expect( dateWithTime( new Date( 1517443200000 ), '2017-03-31:23:59 GMT' ) ).toEqual( '11:59pm 31 March 2017' );
+					expect( dateWithTime( new Date( 1517443200000 ), '2017-03-31:23:59 GMT' ) ).toEqual( '11:59pm 31 March 2017 GMT' );
 				} );
 			} );
 		} );
@@ -46,7 +46,7 @@ describe( 'Date with time filter', function(){
 		it( 'Should use the current date', function(){
 
 			mockDate.set( 1498468857907 );
-			expect( dateWithTime() ).toEqual( '9:20am 26 June 2017' );
+			expect( dateWithTime() ).toEqual( '9:20am 26 June 2017 GMT' );
 			mockDate.reset();
 		} );
 	} );
