348d149e13d9d533a056344351d28fda526aa188
Graham <grahamveal@gmail.com>
2017-12-07 07:27:01 +0000

    Release v6.19.1 (#160)
    * Use description when available for market name for fdi-sector-team-markets view model
    * 6.19.1

--- src/test/app/specs/sub-apps/investment/view-models/fdi-sector-team-markets.spec.js
+++ src/test/app/specs/sub-apps/investment/view-models/fdi-sector-team-markets.spec.js
@@ -1,54 +1,73 @@
 const marketsViewModel = require( '../../../../../../app/sub-apps/investment/view-models/fdi-sector-team-markets' );
 const getBackendStub = require( '../../../../helpers/get-backend-stub' );
 
-const input = getBackendStub( '/investment/fdi/sector_teams/sector_team' );
 
 describe( 'FDI Sector Team markets', function(){
 
-	it( 'Should create the correct output', function(){
+	describe( 'With a sector team', function(){
 
-		const output = marketsViewModel.create( input.results.markets );
+		const input = getBackendStub( '/investment/fdi/sector_teams/sector_team' );
 
-		expect( Array.isArray( output ) ).toEqual( true );
+		it( 'Should create the correct output', function(){
 
-		for( let market of output ){
+			const output = marketsViewModel.create( input.results.markets );
 
-			expect( market.id ).toBeDefined();
-			expect( market.name ).toBeDefined();
-			expect( Array.isArray( market.results ) ).toEqual( true );
+			expect( Array.isArray( output ) ).toEqual( true );
 
-			expect( market.results[ 0 ].type ).toEqual( 'Target' );
-			expect( market.results[ 1 ].type ).toEqual( 'Verified' );
-			expect( market.results[ 2 ].type ).toEqual( 'Result confirmed' );
-			expect( market.results[ 3 ].type ).toEqual( 'Total' );
+			for( let market of output ){
 
-			market.results.forEach( ( result ) => {
+				expect( market.id ).toBeDefined();
+				expect( market.name ).toBeDefined();
+				expect( Array.isArray( market.results ) ).toEqual( true );
 
-				expect( result.type ).toBeDefined();
-				expect( result.modifyer ).toBeDefined();
-				expect( result.total.number ).toBeDefined();
-				expect( result.total.percent ).toBeDefined();
-				expect( result.high ).toBeDefined();
-				expect( result.good ).toBeDefined();
-				expect( result.standard ).toBeDefined();
-			} );
-		}
+				expect( market.results[ 0 ].type ).toEqual( 'Target' );
+				expect( market.results[ 1 ].type ).toEqual( 'Verified' );
+				expect( market.results[ 2 ].type ).toEqual( 'Result confirmed' );
+				expect( market.results[ 3 ].type ).toEqual( 'Total' );
+
+				market.results.forEach( ( result ) => {
+
+					expect( result.type ).toBeDefined();
+					expect( result.modifyer ).toBeDefined();
+					expect( result.total.number ).toBeDefined();
+					expect( result.total.percent ).toBeDefined();
+					expect( result.high ).toBeDefined();
+					expect( result.good ).toBeDefined();
+					expect( result.standard ).toBeDefined();
+				} );
+			}
+		} );
+
+		it( 'Should combine the verified and confirmed into a faux total item', function(){
+
+			const output = marketsViewModel.create( input.results.markets );
+
+			for( let market of output ){
+
+				const verified = market.results[ 1 ];
+				const confirmed = market.results[ 2 ];
+				const totals = market.results[ 3 ];
+
+				expect( totals.total.number ).toEqual( verified.total.number + confirmed.total.number );
+				expect( totals.high ).toEqual( verified.high + confirmed.high );
+				expect( totals.good ).toEqual( verified.good + confirmed.good );
+				expect( totals.standard ).toEqual( verified.standard + confirmed.standard );
+			}
+		} );
 	} );
 
-	it( 'Should combine the verified and confirmed into a faux total item', function(){
+	describe( 'With a sector team overview', function(){
 
-		const output = marketsViewModel.create( input.results.markets );
+		it( 'Should use the description for the name', function(){
 
-		for( let market of output ){
+			const input = getBackendStub( '/investment/fdi/sector_teams/overview' );
+			const teams = input.results.sector_teams;
+			const output = marketsViewModel.create( teams );
 
-			const verified = market.results[ 1 ];
-			const confirmed = market.results[ 2 ];
-			const totals = market.results[ 3 ];
+			output.forEach( ( market, index ) => {
 
-			expect( totals.total.number ).toEqual( verified.total.number + confirmed.total.number );
-			expect( totals.high ).toEqual( verified.high + confirmed.high );
-			expect( totals.good ).toEqual( verified.good + confirmed.good );
-			expect( totals.standard ).toEqual( verified.standard + confirmed.standard );
-		}
+				expect( market.name ).toEqual( teams[ index ].description );
+			} );
+		} );
 	} );
 } );
