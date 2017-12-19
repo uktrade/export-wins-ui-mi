76f91a72a264df5aa66a6d4ab9f34271e87c755f
Graham Veal <graham.veal@digital.trade.gov.uk>
2017-12-19 14:27:18 +0000

    Refactor mouting of investment routes to fix login redirect issues

--- src/app/sub-apps/investment/routes.js
+++ src/app/sub-apps/investment/routes.js
@@ -3,30 +3,45 @@ const sectorTeamsController = require( './controllers/controller.sector-teams' )
 const osRegionsController = require( './controllers/controller.overseas-regions' );
 const ukRegionsController = require( './controllers/controller.uk-regions' );
 
-module.exports = function( router/*, isDev*/ ){
+const MOUNT_POINT = '/investment';
 
-	router.get( '/', indexController );
+module.exports = function( router, user/*, isDev*/ ){
 
-	router.get( '/sector-teams/', sectorTeamsController.sectorTeams );
-	router.get( '/sector-teams/:id/', sectorTeamsController.sectorTeam );
-	router.get( '/sector-teams/:id/hvc-performance/', sectorTeamsController.hvcPerformance );
-	router.get( '/sector-teams/:id/non-hvc-performance/', sectorTeamsController.nonHvcPerformance );
-	router.get( '/sector-teams/:id/hvc-projects/', sectorTeamsController.hvcProjects );
-	router.get( '/sector-teams/:id/non-hvc-projects/', sectorTeamsController.nonHvcProjects );
+	//instead of mouting this router at /investment/
+	//just add /investment to the routes
+	//this means that req.url has the full path
+	//which means that we have the correct path to redirect back to for login etc.
 
-	router.get( '/overseas-regions/', osRegionsController.regions );
-	router.get( '/overseas-regions/:id/', osRegionsController.region );
-	router.get( '/overseas-regions/:id/hvc-performance/', osRegionsController.hvcPerformance );
-	router.get( '/overseas-regions/:id/non-hvc-performance/', osRegionsController.nonHvcPerformance );
-	router.get( '/overseas-regions/:id/hvc-projects/', osRegionsController.hvcProjects );
-	router.get( '/overseas-regions/:id/non-hvc-projects/', osRegionsController.nonHvcProjects );
+	function get( path, ...args ){
 
-	router.get( '/uk-regions/', ukRegionsController.regions );
-	router.get( '/uk-regions/:id/', ukRegionsController.region );
-	router.get( '/uk-regions/:id/hvc-performance/', ukRegionsController.hvcPerformance );
-	router.get( '/uk-regions/:id/non-hvc-performance/', ukRegionsController.nonHvcPerformance );
-	router.get( '/uk-regions/:id/hvc-projects/', ukRegionsController.hvcProjects );
-	router.get( '/uk-regions/:id/non-hvc-projects/', ukRegionsController.nonHvcProjects );
+		//ensure the user middleware gets run for each route
+		args.unshift( user );
+
+		router.get( ( MOUNT_POINT + path ), ...args );
+	}
+
+	get( '/', indexController );
+
+	get( '/sector-teams/', sectorTeamsController.sectorTeams );
+	get( '/sector-teams/:id/', sectorTeamsController.sectorTeam );
+	get( '/sector-teams/:id/hvc-performance/', sectorTeamsController.hvcPerformance );
+	get( '/sector-teams/:id/non-hvc-performance/', sectorTeamsController.nonHvcPerformance );
+	get( '/sector-teams/:id/hvc-projects/', sectorTeamsController.hvcProjects );
+	get( '/sector-teams/:id/non-hvc-projects/', sectorTeamsController.nonHvcProjects );
+
+	get( '/overseas-regions/', osRegionsController.regions );
+	get( '/overseas-regions/:id/', osRegionsController.region );
+	get( '/overseas-regions/:id/hvc-performance/', osRegionsController.hvcPerformance );
+	get( '/overseas-regions/:id/non-hvc-performance/', osRegionsController.nonHvcPerformance );
+	get( '/overseas-regions/:id/hvc-projects/', osRegionsController.hvcProjects );
+	get( '/overseas-regions/:id/non-hvc-projects/', osRegionsController.nonHvcProjects );
+
+	get( '/uk-regions/', ukRegionsController.regions );
+	get( '/uk-regions/:id/', ukRegionsController.region );
+	get( '/uk-regions/:id/hvc-performance/', ukRegionsController.hvcPerformance );
+	get( '/uk-regions/:id/non-hvc-performance/', ukRegionsController.nonHvcPerformance );
+	get( '/uk-regions/:id/hvc-projects/', ukRegionsController.hvcProjects );
+	get( '/uk-regions/:id/non-hvc-projects/', ukRegionsController.nonHvcProjects );
 
 	return router;
 };
