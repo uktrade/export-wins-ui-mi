7421563fe9fd182095fa46ad4ff3c23271a3cda1
Stuart Axon <stu.axon@gmail.com>
2018-06-05 14:30:11 +0100

    Make per-year customisation a module in globals move the "target/aspiraiton text there".

---
+++ src/app/lib/customise-year.js
@@ -0,0 +1,12 @@
+function target(fyYear) {
+
+	if (fyYear > 2017) {
+		return 'aspiration';
+	} else {
+		return 'target';
+	}
+}
+
+module.exports = {
+	target: target
+};
--- src/app/middleware/globals.js
+++ src/app/middleware/globals.js
@@ -1,5 +1,6 @@
 const urls = require( '../lib/urls' );
 const financialYear = require( '../lib/financial-year' );
+const customiseYear = require( '../lib/customise-year' );
 
 module.exports = function( env ){
 
@@ -9,6 +10,7 @@ module.exports = function( env ){
 		env.addGlobal( 'urls', urls( req ) );
 		env.addGlobal( 'currentFy', financialYear.getCurrent() );
 		env.addGlobal( 'selectedYear', Number( req.year ) );
+		env.addGlobal( 'target', customiseYear.target( req.year ));
 
 		next();
 	};
