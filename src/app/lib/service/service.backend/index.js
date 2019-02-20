40d2178a22c074ad15f886036de1fa79dbfa808d
John Fieldman <hello@johnfieldman.com>
2019-02-20 11:16:18 +0000

    fixup! added logger

--- src/app/lib/service/service.backend/index.js
+++ src/app/lib/service/service.backend/index.js
@@ -43,7 +43,8 @@ module.exports = {
 			const user = info.data;
 
 			console.log('333333333333333333333333333333333333333333333');
-			console.log(user);
+			console.log(JSON.stringify(user.permitted_applications));
+			console.log(JSON.parse(user.permitted_applications));
 			console.log('333333333333333333333333333333333333333333333');
 
 			user.internal = ( config.isDev || !!~internalUsers.indexOf( user.email ) );
