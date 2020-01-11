10663d76ca66da41c19a82fa1f59e4798132672f
Mark Henwood <mcbhenwood@icloud.com>
2020-01-11 12:29:05 +0000

    Make tests wait for related processes to end
    Following upgrade of Node et al, these tests were exerting
    their expectations before the controller callback functions
    had completed.

--- src/app/controllers/controller.login.js
+++ src/app/controllers/controller.login.js
@@ -42,7 +42,7 @@ module.exports = {
 
 	oauth: function( req, res ){
 
-		backendService.getOauthUrl( req.query.next ).then( ( info ) => {
+		return backendService.getOauthUrl( req.query.next ).then( ( info ) => {
 
 			const json = info.data;
 
@@ -75,7 +75,7 @@ module.exports = {
 
 		} else if( code.length < MAX_LEN && state.length < MAX_LEN && isAlpha.test( code ) && isAlpha.test( state ) ){
 
-			backendService.postOauthCallback( `code=${ code }&state=${ state }` ).then( ( info ) => {
+			return backendService.postOauthCallback( `code=${ code }&state=${ state }` ).then( ( info ) => {
 
 				const response = info.response;
 				const data = info.data;
