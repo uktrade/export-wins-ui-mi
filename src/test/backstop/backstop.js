632853fe7545e41fe4989651aa8882e6db61843e
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2020-07-13 09:24:00 +0100

    Remove Backstop

--- src/test/backstop/backstop.js
+++ src/test/backstop/backstop.js
@@ -1,71 +1,75 @@
+/*
+	BackstopJS has been removed for security reasons, so it is not currently possible to run these tests.
+*/
+
 const paths = [
-	[ '/2016/', 'Homepage', [] ],
+	['/2016/', 'Homepage', []],
 
-	[ '/downloads/', 'MI - Downloads', [] ],
+	['/downloads/', 'MI - Downloads', []],
 
-	[ '/2016/select-date/', 'Select Year for Dates', [] ],
-	[ '/2016/select-date/2017/', 'Select Dates', [] ],
+	['/2016/select-date/', 'Select Year for Dates', []],
+	['/2016/select-date/2017/', 'Select Dates', []],
 
-	[ '/2016/sector-teams/', 'Sector Teams list', [] ],
-	[ '/2016/sector-teams/overview/', 'Sector Teams Review', [ '.column-date' ] ],
-	[ '/2016/sector-teams/1/', 'Sector team detail', [ '.page-date' ] ],
-	[ '/2016/sector-teams/1/wins/', 'Sector team HVC wins', [ '.page-date' ] ],
-	[ '/2016/sector-teams/1/non-hvc-wins/', 'Sector team non HVC wins', [ '.page-date' ] ],
+	['/2016/sector-teams/', 'Sector Teams list', []],
+	['/2016/sector-teams/overview/', 'Sector Teams Review', ['.column-date']],
+	['/2016/sector-teams/1/', 'Sector team detail', ['.page-date']],
+	['/2016/sector-teams/1/wins/', 'Sector team HVC wins', ['.page-date']],
+	['/2016/sector-teams/1/non-hvc-wins/', 'Sector team non HVC wins', ['.page-date']],
 
-	[ '/2016/hvc-groups/', 'HVC Group list', [] ],
-	[ '/2016/hvc-groups/1/', 'HVC Group detail', [ '.page-date' ] ],
-	[ '/2016/hvc-groups/1/wins/', 'HVC Group HVC wins', [ '.page-date' ] ],
+	['/2016/hvc-groups/', 'HVC Group list', []],
+	['/2016/hvc-groups/1/', 'HVC Group detail', ['.page-date']],
+	['/2016/hvc-groups/1/wins/', 'HVC Group HVC wins', ['.page-date']],
 
-	[ '/2016/overseas-regions/', 'Overseas regions list', [ '.page-date' ] ],
-	[ '/2016/overseas-regions/overview/', 'Overseas regions review', [ '.page-date' ] ],
-	[ '/2016/overseas-regions/1/', 'Overseas region detail', [ '.page-date' ] ],
-	[ '/2016/overseas-regions/1/wins/', 'Overseas region HVC wins', [ '.page-date' ] ],
-	[ '/2016/overseas-regions/1/non-hvc-wins/', 'Overseas region non-hvc-wins', [ '.page-date' ] ],
+	['/2016/overseas-regions/', 'Overseas regions list', ['.page-date']],
+	['/2016/overseas-regions/overview/', 'Overseas regions review', ['.page-date']],
+	['/2016/overseas-regions/1/', 'Overseas region detail', ['.page-date']],
+	['/2016/overseas-regions/1/wins/', 'Overseas region HVC wins', ['.page-date']],
+	['/2016/overseas-regions/1/non-hvc-wins/', 'Overseas region non-hvc-wins', ['.page-date']],
 
-	[ '/2016/hvc/E100/', 'HVC detail', [ '.page-date' ] ],
-	[ '/2016/hvc/E100/wins/', 'HVC wins', [ '.page-date' ] ],
+	['/2016/hvc/E100/', 'HVC detail', ['.page-date']],
+	['/2016/hvc/E100/wins/', 'HVC wins', ['.page-date']],
 
-	[ '/2016/countries/', 'Countries list', [ '.page-date' ] ],
-	[ '/2016/countries/AU/', 'Country detail', [ '.page-date' ] ],
-	[ '/2016/countries/AU/wins/', 'Country HVC wins', [ '.page-date' ] ],
-	[ '/2016/countries/AU/non-hvc-wins/', 'Country non HVC wins', [ '.page-date' ] ],
+	['/2016/countries/', 'Countries list', ['.page-date']],
+	['/2016/countries/AU/', 'Country detail', ['.page-date']],
+	['/2016/countries/AU/wins/', 'Country HVC wins', ['.page-date']],
+	['/2016/countries/AU/non-hvc-wins/', 'Country non HVC wins', ['.page-date']],
 
-	[ '/2016/posts/', 'Posts list', [ '.page-date' ] ],
-	[ '/2016/posts/australia-sydney/', 'Post detail', [ '.page-date' ] ],
-	[ '/2016/posts/australia-sydney/wins/', 'Post HVC wins', [ '.page-date' ] ],
-	[ '/2016/posts/australia-sydney/non-hvc-wins/', 'Post non HVC wins', [ '.page-date' ] ],
+	['/2016/posts/', 'Posts list', ['.page-date']],
+	['/2016/posts/australia-sydney/', 'Post detail', ['.page-date']],
+	['/2016/posts/australia-sydney/wins/', 'Post HVC wins', ['.page-date']],
+	['/2016/posts/australia-sydney/non-hvc-wins/', 'Post non HVC wins', ['.page-date']],
 
-	[ '/2016/uk-regions/', 'UK Regions overview', [ '.page-date' ] ],
-	[ '/2016/uk-regions/north-west/', ' UK Region detail', [ '.page-date', '.sub-heading-date' ] ],
-	[ '/2016/uk-regions/north-west/wins/', ' UK Region HVC wins', [ '.page-date' ] ],
-	[ '/2016/uk-regions/north-west/non-hvc-wins/', ' UK Region Non HVC wins', [ '.page-date' ] ],
+	['/2016/uk-regions/', 'UK Regions overview', ['.page-date']],
+	['/2016/uk-regions/north-west/', ' UK Region detail', ['.page-date', '.sub-heading-date']],
+	['/2016/uk-regions/north-west/wins/', ' UK Region HVC wins', ['.page-date']],
+	['/2016/uk-regions/north-west/non-hvc-wins/', ' UK Region Non HVC wins', ['.page-date']],
 
-	[ '/2016/investment/', 'Investment Homepage', [ '.fdi-overview-date', '.fdi-overview-yoy-date' ] ],
-	[ '/2016/investment/sector-teams/1/', 'Investment - Sector team performance', [ '.page-date' ] ],
-	[ '/2016/investment/sector-teams/1/hvc-performance/', 'Investment - Sector team HVC performance', [ '.page-date' ] ],
-	[ '/2016/investment/sector-teams/1/non-hvc-performance/', 'Investment - Sector team Non HVC performance', [ '.page-date' ] ],
-	[ '/2016/investment/sector-teams/1/hvc-projects/', 'Investment - Sector team HVC projects', [ '.page-date' ] ],
-	[ '/2016/investment/sector-teams/1/non-hvc-projects/', 'Investment - Sector team Non HVC projects', [ '.page-date' ] ]
+	['/2016/investment/', 'Investment Homepage', ['.fdi-overview-date', '.fdi-overview-yoy-date']],
+	['/2016/investment/sector-teams/1/', 'Investment - Sector team performance', ['.page-date']],
+	['/2016/investment/sector-teams/1/hvc-performance/', 'Investment - Sector team HVC performance', ['.page-date']],
+	['/2016/investment/sector-teams/1/non-hvc-performance/', 'Investment - Sector team Non HVC performance', ['.page-date']],
+	['/2016/investment/sector-teams/1/hvc-projects/', 'Investment - Sector team HVC projects', ['.page-date']],
+	['/2016/investment/sector-teams/1/non-hvc-projects/', 'Investment - Sector team Non HVC projects', ['.page-date']]
 ];
 
-const scenarios = paths.reduce( ( output, pathInfo ) => {
+const scenarios = paths.reduce((output, pathInfo) => {
 
-	const [ path, label, hideSelectors ] = pathInfo;
+	const [path, label, hideSelectors] = pathInfo;
 
-	hideSelectors.push( '.experiments', '.hide-experiments' );
+	hideSelectors.push('.experiments', '.hide-experiments');
 
-	output.push( {
+	output.push({
 		label,
-		url: `http://localhost:9001${ path }`,
-		selectors: [ 'document' ],
+		url: `http://localhost:9001${path}`,
+		selectors: ['document'],
 		hideSelectors,
 		misMatchThreshold: 0,
 		requireSameDimensions: true
-	} );
+	});
 
 	return output;
 
-}, [] );
+}, []);
 
 module.exports = {
 	id: 'dev-test',
