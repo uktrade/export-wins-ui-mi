acf711a9bbbd57b83e8010a0939b460eaeafcdcc
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2020-09-15 09:41:36 +0100

    Fixup! Rewrite controller script

--- src/app/controllers/controller.index.js
+++ src/app/controllers/controller.index.js
@@ -1,7 +1,6 @@
 const exportBackendService = require('../lib/service/service.backend').export;
 const renderError = require('../lib/render-error');
 const globalSummary = require('../lib/view-models/global-summary');
-const removeDuplicateHvcs = require('../lib/view-models/remove-duplicate-hvcs');
 
 module.exports = function (req, res) {
 
@@ -9,7 +8,9 @@ module.exports = function (req, res) {
 		const sectorTeams = data.sectorTeams.results;
 		const overseasRegionGroups = data.overseasRegionGroups.results;
 		const ukRegions = data.ukRegions.results;
-		const globalHvcs = removeDuplicateHvcs.removeDuplicates(data.globalHvcs.results);
+		const globalHvcs = data.globalHvcs.results.filter((hvc, index, self) =>
+			index === self.findIndex((hvc2) => hvc.name === hvc2.name)
+		);
 		const summary = globalSummary.create(data.globalWins);
 		
 		res.render('index.html', { sectorTeams, overseasRegionGroups, ukRegions, globalHvcs, summary });
